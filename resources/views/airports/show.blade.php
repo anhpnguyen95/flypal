b246f94a7e7f260e40799f78e1ef0ca195a7d8f0
Anh Nguyen <anhpnguyen95@gmail.com>
2017-05-03 19:58:55 -0400

    added airport stuff

---
+++ resources/views/airports/show.blade.php
@@ -0,0 +1,29 @@
+@extends ('layout')
+
+@section ('content')
+
+
+<div class="col-sn-8 establishment-detail">
+
+	<h1>{{ $airport->airport_name }}</h1>
+
+			<p>
+
+				<a href="/establishments" >Explore</a>
+
+			</p>
+
+			<p>
+
+				Connect
+
+			</p>
+
+			<p>
+
+				Check flight
+			</p>
+
+</div>
+
+@endsection
--- resources/views/establishment/show.blade.php
+++ resources/views/establishment/show.blade.php
@@ -5,6 +5,7 @@
 
 <div class="col-sn-8 establishment-detail">
 
+
 	<h1>{{ $establishment-> name }}</h1>
 
 			<p>
